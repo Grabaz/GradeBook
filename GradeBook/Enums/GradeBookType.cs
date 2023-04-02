a978c92d63a275e6eb61b104cfcad41f3c4b04fa
Filip Kuś <filip.kus@microsoft.wsei.edu.pl>
2023-04-02 17:56:57 +0200

    Dodaj pliki projektów.

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    // TEST : CreateNewEnumGradeBookTypeTests
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU, 
+        OneToFour, 
+        SixPoint
+    }
+}
